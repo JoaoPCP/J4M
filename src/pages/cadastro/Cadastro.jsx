593b2d7c22556f44f74a73208156edbcd29fad81
KukoBerry <JSSacademica@proton.me>
2025-11-12 15:30:49 -0300

    mudado botão e largura do card

--- src/pages/cadastro/Cadastro.jsx
+++ src/pages/cadastro/Cadastro.jsx
@@ -1,7 +1,7 @@
 function Cadastro() {
   return (
     <div className="flex flex-col gap-10 place-content-center">
-        <div className="flex flex-col justify-center text-center p-10 gap-10 bg-neutral-800 w-300 rounded-xl">
+        <div className="flex flex-col justify-center text-center p-10 gap-10 bg-neutral-800 w-250 rounded-xl">
             <h2 className="text-4xl font-russoOne text-white">Crie sua conta e comece já!</h2>
             <form className="flex flex-col gap-10 align-center font-russoOne text-white">
                 <label for="mail"><b>Email</b></label>
@@ -12,7 +12,7 @@ function Cadastro() {
                 <input type="password" placeholder="Digite aqui..." className="bg-black rounded-md p-3 outline-solid outline-white" name="psw" required/>
                 <label for="pswconf"><b>Confirmar Senha</b></label>
                 <input type="password" placeholder="Digite aqui..." className="bg-black rounded-md p-3 outline-solid outline-white" name="pswconf" required/>
-                <button type="submit" className="uppercase bg-pink drop-shadow-sm drop-shadow-white w-60 p-4 rounded-3xl" >Let's J4M!!</button>
+                <button type="submit" className="cursor-pointer uppercase bg-linear-to-t from-pink-700 to-pink drop-shadow-sm drop-shadow-white w-60 p-4 rounded-3xl" >Let's J4M!!</button>
             </form>
         </div>
     </div>
