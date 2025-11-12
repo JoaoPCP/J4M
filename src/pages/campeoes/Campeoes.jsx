811b6ebd28d311c665bd869e449ee63c5c1d5b43
JoaoPCP <joaopcp23@gmail.com>
2025-11-12 13:46:53 -0300

    feat: search input

--- src/pages/campeoes/Campeoes.jsx
+++ src/pages/campeoes/Campeoes.jsx
@@ -2,17 +2,20 @@ import championCover from "../../assets/CampeoesBase.svg";
 import ChampionCard from "./components/ChampionCard";
 
 function Campeoes() {
-  const champions = Array.from({ length: 10 }, (_, i) => ({
+  const champions = Array.from({ length: 10 }, () => ({
     cover: championCover,
     likes: 15,
   }));
-  console.log(champions);
   return (
     <div className="flex flex-col gap-10">
-      <h2 className="font-orbitron text-3xl text-pink">CAMPEÕES</h2>
+      <div className="flex flex-row items-center justify-between">
+        <h2 className="font-orbitron text-3xl text-pink">CAMPEÕES</h2>
+        <input className="bg-black placeholder:text-gray-400 border-gray-400 border-2 focus:outline-white text-white font-russoOne rounded-4xl h-14 pl-6" placeholder="Pesquisar..."/>
+      </div>
+      
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-y-4">
-        {champions.map((item) => (
-          <ChampionCard champion={item} />
+        {champions.map((item, i) => (
+          <ChampionCard champion={item} key={i} />
         ))}
       </div>
     </div>
